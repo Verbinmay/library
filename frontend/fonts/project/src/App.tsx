6f6a6efaf580fa96af07766261033591558e3fdb
Mark Maistrenko <markomaistrenko@gmail.com>
2024-08-18 22:16:46 +0400

    fonts

---
+++ frontend/fonts/project/src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="sistem-font">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+        possimus, a quia animi, tempora beatae harum facilis magnam culpa rem
+        perferendis cum itaque, ab veritatis. Voluptate rem tenetur consequuntur
+        doloribus.
+      </p>
+      <p className="local-font">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur
+        dolorem, debitis possimus architecto corrupti nesciunt odit eius,
+        inventore maxime laudantium modi quam, qui rerum praesentium veniam
+        commodi neque suscipit autem.
+      </p>
+      <p className="google-font">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum tenetur
+        quos veritatis mollitia laudantium eaque labore ut. Magnam iste unde
+        quasi quo! Corrupti hic harum amet? Sapiente commodi officia a!
+      </p>
+    </div>
+  );
+}
+
+export default App;
